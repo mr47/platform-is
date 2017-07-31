0f19ee549462c282d050c91db1edf10cde0f5228
mr47 <mr4700@gmail.com>
2017-07-31 20:40:13 +0300

    update

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-/*! is-windows v1.0.0 | MIT LICENSE (c) 2015-2016 | https://github.com/jonschlinkert/is-windows */
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD
-    define(factory);
-  } else if (typeof exports === 'object') {
-    // Node.js
-    module.exports = factory;
-  } else {
-    // Browser
-    root.isWindows = factory;
-  }
-}(this, function() {
-  'use strict';
+/*! is-windows v1.0.0 | MIT LICENSE (c) 20177 | https://github.com/mr47/platform-is */
+'use strict';
 
-  return (function isWindows() {
-    if (typeof process === 'undefined' || !process) {
-      return false;
-    }
-    return process.platform === 'win32' ||
-    process.env.OSTYPE === 'cygwin' ||
-    process.env.OSTYPE === 'msys';
-  }());
-}));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.isWindows = isWindows;
+exports.isMac = isMac;
+exports.isLinux = isLinux;
+function isWindows() {
+  if (typeof process === 'undefined' || !process) {
+    return false;
+  }
+  return process.platform === 'win32' || process.env.OSTYPE === 'cygwin' || process.env.OSTYPE === 'msys';
+}
+function isMac() {
+  if (typeof process === 'undefined' || !process) {
+    return false;
+  }
+  return process.platform === 'darwin';
+}
+function isLinux() {
+  if (typeof process === 'undefined' || !process) {
+    return false;
+  }
+  return process.platform === 'linux';
+}
+
+exports.default = isWindows;
--- test.js
+++ test.js
@@ -3,6 +3,8 @@
 require('mocha');
 var assert = require('assert');
 var isWindows = require('./');
+var isMac = require('./').isMac;
+var isLinux = require('./').isLinux;
 
 describe('isWindows', function () {
   it('should return true if the platform is windows:', function () {
@@ -10,6 +12,26 @@ describe('isWindows', function () {
       assert.equal(isWindows(), true);
     } else {
       assert.equal(isWindows(), false);
+    }
+  });
+});
+
+describe('isMac', function () {
+  it('should return true if the platform is darwin:', function () {
+    if (process && process.platform === 'darwin') {
+      assert.equal(isMac(), true);
+    } else {
+      assert.equal(isMac(), false);
+    }
+  });
+});
+
+describe('isLinux', function () {
+  it('should return true if the platform is linux:', function () {
+    if (process && process.platform === 'linux') {
+      assert.equal(isMac(), true);
+    } else {
+      assert.equal(isMac(), false);
     }
   });
 });
